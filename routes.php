407618e13dd26b4d4f6785b49072a9c28f09d6ae
stefandurlesteanu <stefan.durlesteanu@gmail.com>
2020-09-08 18:12:29 +0300

    directory tree

--- routes.php
+++ routes.php
@@ -2,10 +2,10 @@
 
 $router->define([
     'CodecoolerBook' => 'controllers/index.php',
-    'Todolist/add' => 'controllers/add.php',
-    'Todolist/about/culture' => 'controllers/about-culture.php',
-    'Todolist/contact' => 'controllers/contact.php',
-    'Todolist/models/index-model' => 'models/index.model.php',
-    'Todolist/models/index-show-table' => 'models/indexShowTable.model.php',
-    'Todolist/models/addTask' => 'models/add.model.php'
+//    'Todolist/add' => 'controllers/add.php',
+//    'Todolist/about/culture' => 'controllers/about-culture.php',
+//    'Todolist/contact' => 'controllers/contact.php',
+//    'Todolist/models/index-model' => 'models/index.model.php',
+//    'Todolist/models/index-show-table' => 'models/indexShowTable.model.php',
+//    'Todolist/models/addTask' => 'models/add.model.php'
 ]);
