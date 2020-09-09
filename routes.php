79b800643d576a4e5d9878a313285a8f30da3cd6
FlorinCiocirlan <florineless@gmail.com>
2020-09-09 13:49:00 +0300

    declared routes

--- routes.php
+++ routes.php
@@ -3,10 +3,10 @@
 $router->define([
     'codecoolerbook' => 'controllers/index.php',
     'codecoolerbook/register' => 'controllers/register.php',
-//    'Todolist/add' => 'controllers/add.php',
     'codecoolerbook/login' => 'controllers/login.php',
-//    'Todolist/contact' => 'controllers/contact.php',
-//    'Todolist/models/index-model' => 'models/index.model.php',
-//    'Todolist/models/index-show-table' => 'models/indexShowTable.model.php',
+//    'codecoolerbook/wall?id=' => 'controllers/wall.php',
+//    'codecoolerbook/viewprofile?id=' => 'controllers/viewprofile.php',
+//    'codecoolerbook/editprofile?id=' => 'controllers/editprofile.php',
+//    'codecoolerbook/search' => 'controllers/search.php',
 //    'Todolist/models/addTask' => 'models/add.model.php'
 ]);
